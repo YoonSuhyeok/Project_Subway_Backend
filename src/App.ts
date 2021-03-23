9247d8c2eb2623e24705963bf58e23d4dccfa166
윤수혁 <cooltime15@likelion.org>
2021-03-23 13:49:24 +0900

    express init

---
+++ src/App.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+
+class App {
+  public application: express.Application;
+
+  constructor() {
+    this.application = express();
+    this.router();
+  }
+
+  private router(): void {
+    this.application.get('/', (req: express.Request, res: express.Response) => {
+      res.send('hello!');
+    })
+  }
+}
+
+export default App;
