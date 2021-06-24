352d4f5ade1aefc8f40860ab79ad64bf79d9e678
윤수혁 <cooltime15@likelion.org>
2021-06-24 15:01:21 +0900

    임시 저장

--- src/App.ts
+++ src/App.ts
@@ -6,21 +6,41 @@ import recipeCntroller from './routes/recipeCntroller';
 import UserController from './routes/UserController';
 import MenuController from './routes/MenuController';
 import authController from './routes/auth';
+const passport = require('passport');
 
+const session = require('express-session')
+
+declare module 'express-session' {
+  export interface SessionData {
+    userId: string;
+    accessToken: string;
+    refreshToken: string;
+  }
+}
 class App {
   public application: express.Application;
+  public sess: any;
 
   constructor() {
     this.application = express();
     this.application.use(express.json());
     this.application.use(express.urlencoded({ extended : true }));
+    this.application.use(session({
+      secret: '21314',
+      resave: false,
+      saveUninitialized: true,
+    }));
+    this.application.use(passport.initialize());
+    this.application.use(passport.session());
     this.router();
   }
 
   private router(): void {
-    this.application.get('/', (req: express.Request, res: express.Response) => {
-      res.send('hello!');
-    })
+    // this.application.get('/', (req: express.Request, res: express.Response) => {
+    //   let session = req.session;
+    //   session.user = '123';
+    //   return res.json(session);
+    // });
     this.application.use('/allergy', alleryController);
     this.application.use('/bread', breadController);
     this.application.use('/combination', recipeCntroller);
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -10,18 +10,33 @@ const authController: express.Router = express.Router();
 passport.use('kakao', new KakaoStrategy({
     clientID: process.env.KAKAO_RESTAPI_KEY,
     callbackURL: '/auth/kakao/callback'
-}, async (accessToken: string, refreshToken:string, profile:any, done: any)=>{
+}, (accessToken: string, refreshToken:string, profile:any, done: any)=>{
     console.log(accessToken);
-    done(null);
+    done(null, profile);
 }));
+
+passport.serializeUser( (user:any, done:any) => {
+    console.log('serialize');
+    done(null, user.id);
+});
+
+passport.deserializeUser((id:any, done:any) => {
+    console.log(id);
+    done(null, id);
+});
 
 authController.get('/kakao', passport.authenticate('kakao'));
 
-authController.get('kakao/callback', passport.authenticate('kakao', {
-    failureRedirect: 'https://makesand.shop',
+authController.get('/kakao/callback', passport.authenticate('kakao', {
+    failureRedirect: 'http://naver.com',
 }), (req, res) => {
-    res.redirect('https://makesand.shop');
+    res.redirect('http://localhost:3000');
 });
+
+authController.get('/session', (req: express.Request, res: express.Response) => {
+    console.log("hi" + req.session);
+    return res.send(req.session);
+})
 
 /*
     /auth/kakao로 요청시 카카오 전략를 통해서 clientID를 담아 카카오 서버로 인증을 요청한다.
